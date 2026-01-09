2d36ec6ec10fd1e939dbf5862a13b2e55ada162b
MaddyMadhesh77 <141992785+MaddyMadhesh77@users.noreply.github.com>
2026-01-10 01:07:24 +0530

    feat: add placeBid API structure

--- src/main/java/com/project/bidBackend/Controller/BidController.java
+++ src/main/java/com/project/bidBackend/Controller/BidController.java
@@ -1,4 +1,17 @@
 package com.project.bidBackend.Controller;
 
+import com.project.bidBackend.Service.BidService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/bids")
 public class BidController {
+
+    @Autowired
+    BidService bidService;
+
+    public void placeBid(int auctionId, double bidAmount){
+        bidService.placeBid(auctionId,bidAmount);
+    }
+
 }
