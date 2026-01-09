6bad1b4c718c0011fd0ce6045ce9f88efe223990
MaddyMadhesh77 <141992785+MaddyMadhesh77@users.noreply.github.com>
2026-01-09 23:56:14 +0530

    Initial Backend skeleton with model

---
+++ src/main/java/com/project/bidBackend/BidBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.bidBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/bidBackend/Model/Product.java
@@ -0,0 +1,4 @@
+package com.project.bidBackend.Model;
+
+public class Product {
+}
